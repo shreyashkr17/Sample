bdc63799ca5a579ced8c3e397f6fe7e785d91cca
Shreyash Kumar <kumarshreyash39@gmail.com>
2023-03-20 23:40:26 +0530

    Map folder created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from './Components/HomePage/Home';
 import Login from './Components/User/Login';
 import Register from './Components/User/Register';
 import Explore from './Components/Explore/explore';
+import Map from "./Components/Map/Map"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/explore" element={<Explore/>} />
+        <Route path="/map" element={<Map/>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/Components/Map/Map.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./location.js";
+import "./map.css"
+
+const Map = () => {
+  return (
+    <div className='Map'>
+      <div className="map_container">
+
+      </div>
+    </div>
+  )
+}
+
+export default Map
