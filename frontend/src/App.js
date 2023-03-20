ef7764c6c6d033daa04fea35f2a35679746a2c91
Anmol Raj Soni <anmolrajsoni15@gmail.com>
2023-03-20 13:01:04 +0530

    Added longin and register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from "./Components/Navbar/navbar";
 import Search from './Components/Search/Search';
+import Home from './Components/HomePage/Home';
+import Login from './Components/User/Login';
+import Register from './Components/User/Register';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/search" element={<Search />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/Components/User/Login.jsx
@@ -0,0 +1,96 @@
+import { Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+import "./Login.css";
+import Google from "../../Assets/icons/google-oauth.png";
+import Facebook from "../../Assets/icons/fb-oauth.png";
+import Github from "../../Assets/icons/github-oauth.png";
+
+// import { loginUser } from "../../Actions/User";
+// import { useAlert } from "react-alert";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+//   const dispatch = useDispatch();
+//   const alert = useAlert();
+
+//   const {error} = useSelector((state) => state.user);
+//   const {message} = useSelector((state) => state.like);
+
+  const loginHandler = (e) => {
+    // e.preventDefault();
+    // dispatch(loginUser(email, password));
+  };
+  
+//   useEffect(() => {
+//     if (error) {
+//       alert.error(error);
+//       dispatch({ type: "clearErrors" });
+//     }
+//     if (message) {
+//       alert.success(message);
+//       dispatch({ type: "clearMessage" });
+//     }
+    
+//   }, [dispatch, error, alert, message]);
+
+  return (
+    <div className="login">
+      <form className="loginForm" onSubmit={loginHandler}>
+        <Typography variant="h3" style={{ padding: "2vmax" }}>
+          VedicHeal
+        </Typography>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Link to="/forgot/password">
+          <Typography>Forgot Password?</Typography>
+        </Link>
+        <button className="loginButton" type="submit">
+            <Typography>Login</Typography>
+        </button>
+        <Link to="/register">
+          <Typography>New User?</Typography>
+        </Link>
+        <p className="divider">
+            <span className="divider-line"></span>
+            OR
+            <span className="divider-line"></span>
+        </p>
+        <div className="authButtons">
+            <li>
+                <a href="google.com">
+                    <img src={Google} alt="Google" />
+                </a>
+            </li>
+            <li>
+                <a href="facebook.com">
+                    <img src={Facebook} alt="Facebook" />
+                </a>
+            </li>
+            <li>
+                <a href="github.com">
+                    <img src={Github} alt="Github" />
+                </a>
+            </li>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
