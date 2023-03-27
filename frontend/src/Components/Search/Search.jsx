9f30b1a11f8de5d5b24ebb17d47329232b7a4542
Shreyash Kumar <kumarshreyash39@gmail.com>
2023-03-28 00:25:18 +0530

    search engine css added and explore background changed

--- frontend/src/Components/Search/Search.jsx
+++ frontend/src/Components/Search/Search.jsx
@@ -13,6 +13,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { getDisease } from "../../actions/diseaseAction";
 import { useNavigate } from "react-router-dom";
 
+
+import searchLogo from "../../Assets/ayurLogo.png";
+import searchBtn from "../../Assets/search.gif";
+
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
@@ -64,7 +68,51 @@ const Search = () => {
   return (
     <>
       <MetaData title="Search - VedicHeal" />
-      <form className="searchBox" onSubmit={handleSearch}>
+      <div className="searchContainer">
+        <div className="search_logo_container">
+          <div className="left_search_logo">
+            <img src={searchLogo} className="searchLogo" alt="" />
+          </div>
+          <div className="right_search_logo">
+            <h1 className="header_text">VEDICHEAL</h1>
+          </div>
+        </div>
+        <form className="formSearch" onSubmit={handleSearch}>
+          <div className="searchbox_container">
+            <div className="searchBox">
+                <div className="inputSearch">
+                  <input type="text" value={searchQuery} className="searchtext" onChange={handleInputChange} id="" placeholder="e.g. Cough and Cold" />
+                </div>
+                <div className="srchbtnContainer">
+                    <div className="circle_cont">
+                      {/* <img src={searchBtn} className="searchBtn" alt="" type="submit" value="Search" /> */}
+                      <input type="image" src={searchBtn} className="searchBtn" alt="" value="Search" />
+                    </div>
+                </div>
+            </div>
+          </div>
+          {suggestions.length > 0 && (
+          <div className="suggestionData">
+            <ul className="unorderSuggest">
+            {suggestions.map((suggestion) => (
+              <li className="listSuggest" onClick={() => handleSuggestionClick(suggestion)}>
+                {suggestion}
+              </li>
+            ))}
+          </ul>
+          </div>
+        )}
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Search;
+
+
+
+{/* <form className="searchBox" onSubmit={handleSearch}>
         <input
           type="text"
           placeholder="Search the Disease..."
@@ -83,22 +131,4 @@ const Search = () => {
           </ul>
           </div>
         )}
-      </form>
-      {/* <div className="display">
-        <h1>id</h1>
-        <h1>{searchResults.Did}</h1>
-      </div>
-      {searchResults.length > 0 && (
-        <ul>
-          <li>{searchResults.Did}</li>
-          <li>{searchResults.Dname}</li>
-          {searchResults.remedy.map((result) => (
-            <li>{result}</li>
-          ))}
-        </ul>
-      )} */}
-    </>
-  );
-};
-
-export default Search;
+      </form> */}
